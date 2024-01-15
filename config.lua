f78edc977616f668697ade8b731895232daaebd8
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-01-15 05:51:23 -0600

    Version 1.0.0 Release

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {};
+Config.Main = {
+	permissionBased = true, -- Enable / Disable permissions for the script (Default = true) (add_ace group.staff simple.announce allow)
+	loggingWebhook = "" -- Channel that announcements will be logged to.
+};
