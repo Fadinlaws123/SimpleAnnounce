d2125bf28f9b1a5777e524c12a3db4ec1c722006
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-01-22 08:14:29 -0600

    Version 1.0.1 Release

--- server/server.lua
+++ server/server.lua
@@ -118,7 +118,7 @@ function sendDiscord(name, message)
 	local content = {
         {
         	["color"] =  Config.Announce.EmbedConfig.EmbedColor,
-          ["title"] = "**Server Announcement by: __Fadin_laws__**",
+          ["title"] = "**Server Announcement by: __" .. GetPlayerName(source) .. "**",
           fields = {
             { name = "**→ Announcement:**", value = "*" .. message .. "*"},
             { name = "**→ Command Used:** __/announce__", value = ""},
