b655c34d52219ddebe7b2aa1b153dd217dd2481f
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-01-22 08:13:49 -0600

    Version 1.0.1 Release

--- config.lua
+++ config.lua
@@ -10,12 +10,12 @@ Config.Announce = {
 
 	LogSettings = {
 		EnableDiscordLogs = true, -- Enable / Disable the usage of logging via discord.
-		loggingWebhook = "https://discord.com/api/webhooks/1195891785449345075/qZi9KUDgmrRPqB4yy5x795IPvLnEhQQGmoW3BxdOBvEIt46fj8wCXkeWyEMzNG-ISznJ", -- Webhook that logs will be sent to. (EnableDiscordLogs must be enabled!)
+		loggingWebhook = "CHANGE ME", -- Webhook that logs will be sent to. (EnableDiscordLogs must be enabled!)
 	},
 
 	EmbedConfig = {
 		EmbedColor = "5263615", -- Color of the embed, can be Hex or decimal code.
-		WebhookPicture = "https://i.seadn.io/gae/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc?auto=format&dpr=1&w=1000" -- Picture that shows for the webhook logs.
+		WebhookPicture = "CHANGE ME" -- Picture that shows for the webhook logs.
 		-- More settings coming soon --
 	}
 }
