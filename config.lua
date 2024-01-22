0f150ed619c69f2c50518a8e3555ee0cd93a1cc5
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-01-22 08:30:33 -0600

    Version 1.0.1 Release

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config.Announce = {
 
 	Settings = {
 		Style = "2", -- Change the style of how you want to have /announce messages to appear! (Available options: 1 | 2)
-		UsePermissions = false, -- Enable / Disable the need for permissions.
+		UsePermissions = true, -- Enable / Disable the need for permissions.
 		AcePerm = "simple.announce", -- Permission name for using /announce
 	},
 
