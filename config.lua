98e82d0a4e655f48940ba85eae6a725659d459cf
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-01-22 07:58:50 -0600

    Delete config.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-Config = {};
-Config.Main = {
-	permissionBased = true, -- Enable / Disable permissions for the script (Default = true) (add_ace group.staff simple.announce allow)
-	loggingWebhook = "" -- Channel that announcements will be logged to.
-};
